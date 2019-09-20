79de12860ea65066d2e203f42a0d57ff3b7068a1
yangyiusa <43105474+yangyiusa@users.noreply.github.com>
2019-09-19 18:48:42 -0700

    Add files via upload

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -1,27 +1,48 @@
-/**
- * Unit 1 Project
- * 
- * In this project you have the opportunity to create a piece of artwork using ASCII 
- * (American Standard Code for Information Interchange). Using the already created file 
- * named `ASCIIArt.java` write a program that outputs your artwork to the console. 
- * The goal of this project is to use ONLY what we have learned in Unit 1 to create 
- * anything you would like so be creative (and school appropriate). Artwork must also have a caption describing what it's depicting.  
- * 
- * For those of you who have dabbled in coding before, try to contain your code to 
- * just what we have discussed in this unit.  
- * 
- * @author Mr. Rasmussen 
- *
- */
-
-// YOU CODE HERE :) 
 
 public class ASCIIArt {
-	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		drawWall();
+		drawBase();
+		System.out.println("\\_/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\_/");
+		System.out.println("  \\_/\\/\\/\\/\\/\\/\\/\\/\\/\\_/");
+		System.out.println("    \\_/\\/\\/\\/\\/\\/\\/\\_/");
+		System.out.println("      \\_/\\/\\/\\/\\/\\_/");
+		drawWall();
+		drawBody();
+		drawBase();
+		System.out.println("A Space Needle shows its love to Computer Science.");
+	}
+	public static void drawBase() {
+		System.out.println("         __/||\\__");
+		System.out.println("      __/:::||:::\\__");
+		System.out.println("   __/::::::||::::::\\__");
+		System.out.println("__/:::::::::||:::::::::\\__");
+		System.out.println("|\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"|");
+	}
+	public static void drawBody() {
+		System.out.println("        |%%|AP|%%|");
+		System.out.println("        |%%|CS|%%|");
+		System.out.println("       |%%|ROCK|%%|");
+		System.out.println("         |%%||%%|");
+		System.out.println("         |%%||%%|");
+		System.out.println("         |%%||%%|");
+		System.out.println("         |%%||%%|");
+		System.out.println("         |%%||%%|");
+		System.out.println("         |%%||%%|");
+		System.out.println("         |%%||%%|");
+		System.out.println("         |%%||%%|");
+		System.out.println("         |%%||%%|");
+		System.out.println("         |%%||%%|");
+		System.out.println("         |%%||%%|");
+		System.out.println("         |%%||%%|");
+		System.out.println("         |%%||%%|");
+	}
+	public static void drawWall() {
+		System.out.println("            ||");
+		System.out.println("            ||");
+		System.out.println("            ||");
+		System.out.println("            ||");
 	}
 }
